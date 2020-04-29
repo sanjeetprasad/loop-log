6d91e23cd4ddfcfc4356cff549eeea8e0ca47bdb
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-04-29 14:47:04 -0500

    loop-log solved

---
+++ main.js
@@ -0,0 +1,31 @@
+//Loop through this array of objects and console.log //
+//"name's job title is title."//
+
+const array = [
+  {
+    name: "Bob",
+    department: "sales",
+    title: "sales manager"
+  },
+  {
+    name: "Tina",
+    department: "finance",
+    title: "director of finance"
+  },
+  {
+    name: "Randy",
+    department: "IT",
+    title: "hardware guy"
+  },
+  {
+    name: "Glenda",
+    department: "C-suite",
+    title: "CEO"
+  }
+]
+
+for (let i = 0; i < array.length; i++) {
+    console.log(`${array[i].name}'s job title is ${array[i].title}.`)
+}
+    
+    
